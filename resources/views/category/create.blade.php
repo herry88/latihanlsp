f4ced943867be8ad72247618f280f56dd0baad1f
Herry Prasetyo <herry081288@gmail.com>
2023-01-10 13:17:27 +0700

    categoryStoreForm

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -7,18 +7,22 @@
                 <div class="card">
                     <div class="card-header">Tambah Kategori</div>
                     <div class="card-body">
-                        <div class="form-group">
-                            <label for="">Nama Kategori</label>
-                            <input type="text" name="namacategory" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="">Deskripsi</label>
-                            <input type="text" name="deskripsi" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-primary" type="submit">Simpan Data</button>
-                        </div>
+                        <form action="{{ route('category.store') }}" method="post">
+                            @csrf
+                            @method('POST')
+                            <div class="form-group">
+                                <label for="">Nama Kategori</label>
+                                <input type="text" name="namacategory" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Deskripsi</label>
+                                <input type="text" name="deskripsi" class="form-control">
+                            </div>
                     </div>
+                    <div class="card-footer">
+                        <button class="btn btn-primary">Simpan</button>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
