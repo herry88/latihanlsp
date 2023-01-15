26f468eb708064362d5cd5283a377ae7522ae43d
Herry Prasetyo <herry081288@gmail.com>
2023-01-15 12:26:53 +0700

    index

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,7 +7,17 @@
                 <div class="card">
                     <div class="card-header"><a href="" class="btn btn-primary">Add</a></div>
                     <div class="card-body">
-
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>namaproduct</th>
+                                    <th>Nama Kategori</th>
+                                    <th>qty</th>
+                                    <th>Tools</th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
                 </div>
             </div>
