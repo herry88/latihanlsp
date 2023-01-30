7a9654ee00f48e5fc60b0ef19dafb389335ee4ea
Herry Prasetyo <herry081288@gmail.com>
2023-01-30 10:26:51 +0700

    seederUser

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //ini untuk dummy data Admin
+        $users = [
+            [
+                'name'=>'admin',
+                'email'=>'admin@seeder.com',
+                'password'=>Hash::make('passwordadmin'),
+                'level'=>'admin',
+                'email_verified_at'=>now()
+            ],
+            [
+                'name'=>'user',
+                'email'=>'user@seeder.com',
+                'password'=>Hash::make('passworduser'),
+                'level'=>'customer',
+                'email_verified_at'=>now()
+            ]
+    ];
+    //eksekusi
+    foreach ($users as $user) {
+        User::create($user);
+    }
+    }
+}
